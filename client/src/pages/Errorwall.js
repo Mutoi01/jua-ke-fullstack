f021311092d17fa07943e7bb5bac7d2fd097b52b
Mutoi01 <pngando9@gmail.com>
2022-10-14 09:36:25 +0300

    add failed authorisation error message page

---
+++ client/src/pages/Errorwall.js
@@ -0,0 +1,13 @@
+function Errorwall() {
+  return (
+    <article>
+      <h2>Maximum articles viewed</h2>
+      <p>
+        You've reached your limit for free articles! Create an account to see
+        more great content.
+      </p>
+    </article>
+  );
+}
+
+export default Errorwall;
