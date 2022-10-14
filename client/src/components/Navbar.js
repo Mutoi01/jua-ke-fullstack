ea120db2ed406fb2d35adce84bf14b28db944fb0
Mutoi01 <pngando9@gmail.com>
2022-10-14 09:31:41 +0300

    add navbar component

---
+++ client/src/components/Navbar.js
@@ -0,0 +1,38 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+import '../res/nav.css'
+
+function Navbar() {
+  return (
+    <>
+     <header>
+        
+        <a href="#" className="logo">Jua.KE</a>
+    
+        <nav className="navbar">
+          <ul>
+             <li><Link to={"/"} >Home</Link></li>
+             
+             <li><Link to={"/"} >today's special</Link> </li>
+
+             <li><Link to={"/"} >contact us</Link></li>
+
+             <li><Link to={"/login"} >Login</Link>
+             <ul>
+                      <li> <Link to={"/"} >logout</Link></li>
+                    </ul>
+             </li>
+            
+             {/* <li><Link to={"/staff"} > Port Operators +</Link>
+                    <ul>
+                      <li> <Link to={"/add-staff"} >Add Staff</Link></li>
+                    </ul>
+             </li> */}
+           </ul>
+        </nav>
+      </header>
+    </>
+  )
+}
+
+export default Navbar
